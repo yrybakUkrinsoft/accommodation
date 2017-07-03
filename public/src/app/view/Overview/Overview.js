f3483b83e0b4585c85fa945f5b3810face6e6361
woolfi182 <wmornotwm@gmail.com>
2017-07-03 19:39:24 +0300

    add migration

--- public/src/app/view/Overview/Overview.js
+++ public/src/app/view/Overview/Overview.js
@@ -1,32 +1,33 @@
-import React from "react";
+import React from 'react';
+import {ButtonToolbar, Button} from 'react-bootstrap';
 
 export default () => {
     return (
         <main className="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
             <h1>Dashboard</h1>
 
-            <section className="row text-center placeholders">
-                <div className="col-6 col-sm-3 placeholder">
-                    <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" className="img-fluid rounded-circle" alt="Generic placeholder thumbnail"/>
-                        <h4>Label</h4>
-                        <div className="text-muted">Something else</div>
-                </div>
-                <div className="col-6 col-sm-3 placeholder">
-                    <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" className="img-fluid rounded-circle" alt="Generic placeholder thumbnail"/>
-                        <h4>Label</h4>
-                        <span className="text-muted">Something else</span>
-                </div>
-                <div className="col-6 col-sm-3 placeholder">
-                    <img src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" className="img-fluid rounded-circle" alt="Generic placeholder thumbnail"/>
-                        <h4>Label</h4>
-                        <span className="text-muted">Something else</span>
-                </div>
-                <div className="col-6 col-sm-3 placeholder">
-                    <img src="data:image/gif;base64,R0lGODlhAQABAIABAADcgwAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" className="img-fluid rounded-circle" alt="Generic placeholder thumbnail"/>
-                        <h4>Label</h4>
-                        <span className="text-muted">Something else</span>
-                </div>
-            </section>
+            <ButtonToolbar>
+                {/* Standard button */}
+                <Button>Default</Button>
+
+                {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+                <Button bsStyle="primary">Primary</Button>
+
+                {/* Indicates a successful or positive action */}
+                <Button bsStyle="success">Success</Button>
+
+                {/* Contextual button for informational alert messages */}
+                <Button bsStyle="info">Info</Button>
+
+                {/* Indicates caution should be taken with this action */}
+                <Button bsStyle="warning">Warning</Button>
+
+                {/* Indicates a dangerous or potentially negative action */}
+                <Button bsStyle="danger">Danger</Button>
+
+                {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+                <Button bsStyle="link">Link</Button>
+            </ButtonToolbar>
 
             <h2>Section title</h2>
             <div className="table-responsive">
